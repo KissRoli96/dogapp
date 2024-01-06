4a4f6830ac6b5f689a73311efa3481661ee323c9
Kiss Roland <kissrolandkristof@gmail.com>
2024-01-06 18:57:05 +0100

    MongoDB adatbázis csatlakozás kiszervezése

---
+++ backend/src/config/mongoose.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB csatlakozási hiba:'));
+db.once('open', () => {
+  console.log('A MongoDB sikeresen csatlakozott!');
+});
+
+module.exports = mongoose;
