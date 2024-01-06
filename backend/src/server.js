21f027c36570afd69d497e503d2dacdfb98a8eef
Kiss Roland <kissrolandkristof@gmail.com>
2024-01-06 18:56:41 +0100

    server.js refakt

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,25 +1,25 @@
 // Express inicializálása
 const express = require('express');
+const bodyParser = require('./middleware/bodyParser');
+const mongoose = require('./config/mongoose');
+const cors = require('cors');
+const router = require('./routes/api');
+
 const app = express();
 
 // Middleware-ek hozzáadása
-//    - express.json() middleware az incoming JSON adatok könnyű olvashatóságához
-app.use(express.json());
-
-/* CORS engedélyezése (fejlesztési környezetben)
-- CORS engedélyezése, hogy a frontend hozzáférhessen a backend erőforrásokhoz
-*/
-const cors = require('cors');
+app.use(bodyParser);  // Beépített JSON body parser
+// - CORS engedélyezése, hogy a frontend hozzáférhessen a backend erőforrásokhoz
 app.use(cors());
+
+// Statikus fájlok szolgáltatása a "public" mappából
+app.use(express.static('public'));
 
 // Útválasztók beállítása
 //    - Importálni az API útválasztókat (routes/api.js)
 const apiRoutes = require('./routes/api');
 //    - Az '/api' prefix alatt lesznek kezelve az  API hívások
 app.use('/api', apiRoutes);
-
-// Statikus fájlok szolgáltatása (opcionális)
-app.use(express.static('public'));
 
 // Port beállítása és szerver indítása
 const PORT = process.env.PORT || 3000;
