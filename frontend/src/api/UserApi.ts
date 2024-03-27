01ba339b6faa16ca3e2850959172aa26191fc996
Kiss Roland <kissrolandkristof@gmail.com>
2024-03-27 20:59:28 +0100

    Userrel kapcsolatos eljárások kiszervezése UserApi fájlba

---
+++ frontend/src/api/UserApi.ts
@@ -0,0 +1,46 @@
+import api from './api';
+
+export const getUsers = async () => {
+    try {
+      const response = await api.get('/users');
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  };
+  
+  export const getUser = async (id: string) => {
+    try {
+      const response = await api.get(`/users/${id}`);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  };
+  
+  export const createUser = async (userData: any) => {
+    try {
+      const response = await api.post('/users', userData);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  };
+  
+  export const updateUser = async (id: string, userData: any) => {
+    try {
+      const response = await api.put(`/users/${id}`, userData);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  };
+  
+  export const deleteUser = async (id: string) => {
+    try {
+      const response = await api.delete(`/users/${id}`);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  };
