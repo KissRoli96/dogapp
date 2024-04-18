d792f670a59bb554037eafee9573a4e256fdb233
Kiss Roland <kissrolandkristof@gmail.com>
2024-04-18 21:00:02 +0200

    időpontfoglalás modell hozzáadas

---
+++ backend/src/models/appointment.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const appointmentSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  time: {
+    type: String,
+    required: true
+  },
+  duration: {
+    type: Number,
+    required: true
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'confirmed', 'cancelled'],
+    default: 'pending'
+  },
+  notes: String
+});
+
+const Appointment = mongoose.model('Appointment', appointmentSchema);
+
+module.exports = Appointment;
