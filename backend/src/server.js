7adfd7d86ce3be25bd7cb2c86d9dffb31a7f8c0a
Kiss Roland <kissrolandkristof@gmail.com>
2024-05-11 14:20:30 +0200

    Keycloak feature added

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,8 @@ const bodyParser = require('./middleware/bodyParser');
 const mongoose = require('./config/mongoose');
 const cors = require('cors');
 const router = require('./routes/api.js');
+// const session = require('express-session');
+// const Keycloak = require('keycloak-connect');
 
 const app = express();
 
@@ -12,6 +14,27 @@ app.use(bodyParser);  // Beépített JSON body parser
 // - CORS engedélyezése, hogy a frontend hozzáférhessen a backend erőforrásokhoz
 app.use(cors());
 
+// Keycloak setup
+// const memoryStore = new session.MemoryStore();
+// app.use(session({
+//   secret: 'some secret',
+//   resave: false,
+//   saveUninitialized: true,
+//   store: memoryStore
+// }));
+
+
+// const keycloak = new Keycloak({ store: memoryStore }, {
+//   "realm": "myrealm",
+//   "auth-server-url": "http://localhost:8080",
+//   "ssl-required": "external",
+//   "resource": "myclient",
+//   "public-client": true,
+//   "confidential-port": 0
+// });
+
+// app.use(keycloak.middleware());
+
 // Statikus fájlok szolgáltatása a "public" mappából
 app.use(express.static('public'));
 
@@ -19,7 +42,8 @@ app.use(express.static('public'));
 //    - Importálni az API útválasztókat (routes/api.js)
 const apiRoutes = require('./routes/api');
 //    - Az '/api' prefix alatt lesznek kezelve az  API hívások
-app.use('/api', apiRoutes);
+// app.use('/api', keycloak.protect(), apiRoutes); // Protect API routes with Keycloak
+app.use('/api', apiRoutes); // API routes witwhout keylcoak
 
 // Port beállítása és szerver indítása
 const PORT = process.env.PORT || 3000;
