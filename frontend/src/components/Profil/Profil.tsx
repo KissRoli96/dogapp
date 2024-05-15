ff10a94cbd27e7cc8cf423b2139b61e64ae1dda3
Kiss Roland <kissrolandkristof@gmail.com>
2024-05-15 20:32:51 +0200

    Profil component refact

--- frontend/src/components/Profil/Profil.tsx
+++ frontend/src/components/Profil/Profil.tsx
@@ -1,10 +1,13 @@
-import React, { Suspense } from 'react';
-import { Avatar, Typography, Grid, Paper, makeStyles, Button } from '@material-ui/core';
+import React, { Suspense, useEffect, useState } from 'react';
+import { Avatar, Typography, Grid, Paper, makeStyles, Button, CardMedia } from '@material-ui/core';
 import { useParams } from 'react-router-dom';
 import useFetchUserById from '../../api/useFetchUserById';
 import { Dog, User, Appointment, AppointmentStatus, Service } from '../../types/types';
-
-
+import useFetchDogsByUserId from '../../api/useFetchDogsByOwnerId';
+import { getDogPicture } from '../../api/dogApi';
+import useFetchAppointmentsByUserId from '../../api/useFetchAppointmentsByUserId';
+import useFetchServicesByUserId from '../../api/useFetchServicesByUserId';
+import useFetchReviewByUserId from '../../api/useFetchReviewByUserId';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -20,28 +23,60 @@ const useStyles = makeStyles((theme) => ({
   button: {
     margin: theme.spacing(1),
   },
+  title: {
+    marginTop: theme.spacing(4), // Adjust as needed
+  },
 }));
 
 export const Profile: React.FC = () => {
   const classes = useStyles();
   const { id } = useParams<{ id: string }>();
-  const { data: user, error } = useFetchUserById(id);
+  const [fileUrl, setFileUrl] = useState<string | null>(null);
+
+  const { data: user, error: userError } = useFetchUserById(id);
+  const { data: dogs, error: dogsError } = useFetchDogsByUserId(id);
+  const { data: appointments, error: appointmentsError } = useFetchAppointmentsByUserId(id);
+
+  useEffect(() => {
+    if (user && dogs) {
+      getDogPicture(dogs[0]._id)
+        .then((blob) => {
+          const url = URL.createObjectURL(blob);
+          setFileUrl(url);
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });
+    }
+  }, [user, dogs]);
   
   if (!id) {
     return <div>Invalid user ID</div>;
   }
 
 
-  if (error) {
-    return <div>Error: {error}</div>;
+  if (userError) {
+    return <div>Error: {userError}</div>;
   }
 
   if (!user) {
     return <div>Loading...</div>; // or a loading spinner
   }
 
+  if (!dogs || dogs.length === 0) {
+    return <div>Loading...</div>; // or a loading spinner
+  }
+
+  if (!appointments || appointments.length === 0) { 
+    return <div>Loading...</div>; // or a loading spinner
+  }
+
   const { username, email, profile } = user;
   const { firstName, lastName, age, address } = profile;
+
+  const lastDogs = dogs.slice(-2);
+  const lastAppointments = appointments.slice(-2);
+
 
   const handleEdit = () => {
     // Handle edit action
@@ -51,6 +86,10 @@ export const Profile: React.FC = () => {
     // Handle delete action
   };
   return (
+    <>
+     <Typography variant="h3" gutterBottom className={classes.title}>
+                            Profile:
+                        </Typography>
     <Paper className={classes.paper}>
       <Grid container spacing={2}>
         <Grid item>
@@ -59,7 +98,7 @@ export const Profile: React.FC = () => {
         <Grid item xs={12} sm container>
           <Grid item xs container direction="column" spacing={2}>
             <Grid item xs>
-              <Typography gutterBottom variant="subtitle1">
+              <Typography gutterBottom variant="h6">
                 Fullname: {firstName} {lastName}
               </Typography>
               <Typography variant="body2" gutterBottom>
@@ -87,6 +126,90 @@ export const Profile: React.FC = () => {
         </Grid>
       </Grid>
     </Paper>
+    <Typography variant="h3" gutterBottom className={classes.title}>
+                            Dog(s):
+                        </Typography>
+    {lastDogs.map((dog, index) => (
+      <Paper key={index} className={classes.paper}>
+        <Grid container spacing={2}>
+        <Grid item>
+          <Avatar className={classes.avatar}>{dog.name[0]}</Avatar>
+        </Grid>
+          <Grid item xs={12} sm container>
+            <Grid item xs container direction="column" spacing={2}>
+              <Grid item xs>
+                <Typography gutterBottom variant="h6">
+                  Dog's Name: {dog.name}
+                </Typography>
+                <Typography variant="body2" gutterBottom>
+                  Gender: {dog.gender}
+                </Typography>
+                <Typography variant="body2" color="textSecondary">
+                  Breed: {dog.breed}
+                </Typography>
+                <Typography variant="body2" color="textSecondary">
+                  Age: {dog.age}
+                </Typography>
+                <Typography variant="body2" color="textSecondary">
+                  Weight: {dog.weight} Kg
+                </Typography>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item>
+              <Button variant="contained" color="primary" className={classes.button} onClick={handleEdit}>
+                Edit
+              </Button>
+              <Button variant="contained" color="secondary" className={classes.button} onClick={handleDelete}>
+                Delete Dog
+              </Button>
+            </Grid>
+      </Paper>
+    ))}
+    <Typography variant="h3" gutterBottom className={classes.title}>
+                             Your Appointment(s):
+                        </Typography>
+     {lastAppointments.map((appointment, index) => (
+  <Paper key={index} className={classes.paper}>
+    <Grid container spacing={2}>
+      <Grid item xs={12} sm container>
+        <Grid item xs container direction="column" spacing={2}>
+          <Grid item xs>
+            <Typography gutterBottom variant="h6">
+              Appointment {index + 1}:
+            </Typography>
+            <Typography variant="body2" gutterBottom>
+              Date: {new Date(appointment.date).toDateString()}
+            </Typography>
+            <Typography variant="body2" color="textSecondary">
+              Start Time: {appointment.startTime}
+            </Typography>
+            <Typography variant="body2" color="textSecondary">
+              End Time: {appointment.endTime}
+            </Typography>
+            <Typography variant="body2" color="textSecondary">
+              Status: {appointment.status}
+            </Typography>
+            <Typography variant="body2" color="textSecondary">
+              Notes: {appointment.notes}
+            </Typography>
+            {/* ... rest of appointment's data */}
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+    <Grid item>
+              <Button variant="contained" color="primary" className={classes.button} onClick={handleEdit}>
+                Reschedule Appointment
+              </Button>
+              <Button variant="contained" color="secondary" className={classes.button} onClick={handleDelete}>
+              Cancel Appointment
+              </Button>
+            </Grid>
+  </Paper>
+))}
+    </>
   );
 };
 
